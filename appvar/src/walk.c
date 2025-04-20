43c2e5378a6695ea81f5895ae0d2b8a24985fd7a
Shaun McFall <shaun@shaunmcfall.com>
2025-04-20 17:14:39 -0400

    Rainbow Walk

---
+++ appvar/src/walk.c
@@ -0,0 +1,69 @@
+#include <fileioc.h>
+#include <graphx.h>
+#include <keypadc.h>
+#include <time.h>
+
+#include <sys/rtc.h>
+#include <sys/util.h>
+
+#include "palette.h"
+#include "turtle.h"
+
+#define NUM_TURTLES 15
+#define ANGLE 6
+#define SPEED 9
+
+void walk_init(const uint8_t numTurtles, Turtle turtles[numTurtles]) {
+    for (uint8_t i = 0; i < numTurtles; i++) {
+        Turtle* t = &turtles[i];
+        Turtle_Init(t);
+        Turtle_PenUp(t);
+
+        Turtle_SetColor(t, random() % 255 + 1);
+        Turtle_Goto(t, random() % GFX_LCD_WIDTH, random() % GFX_LCD_HEIGHT);
+        Turtle_SetAngle(t, random());
+        
+        Turtle_SetWrap(t, 1);
+        Turtle_PenDown(t);
+    }
+}
+
+void walk_walk(const uint8_t numTurtles, Turtle turtles[numTurtles]) {
+    for (int i = 0; i < numTurtles; i++) {
+        Turtle* t = &turtles[i];
+        Turtle_Left(t, ANGLE - random() % (ANGLE * 2));
+        Turtle_Forward(t, random() % SPEED + SPEED);
+
+        if (t->x >= 0 && t->x < GFX_LCD_WIDTH
+            && t->y >= 0 && t->y < GFX_LCD_HEIGHT)
+        {
+            gfx_SetColor(t->color);
+            gfx_SetPixel(t->x, t->y);
+        }
+    }
+}
+
+void walk(void) {
+    gfx_Begin();
+    gfx_FillScreen(0);
+
+    gfx_palette[0] = 0;
+    palette_rainbow(gfx_palette);
+
+    srand(rtc_Time());
+    
+    Turtle turtles[NUM_TURTLES];
+    walk_init(NUM_TURTLES, turtles);
+    gfx_SetDrawScreen();
+    
+    uint8_t counter = 0;
+    while (!kb_AnyKey()) {
+        walk_walk(NUM_TURTLES, turtles);
+        palette_shift(gfx_palette);
+        counter++;
+        if (counter == 0)
+            gfx_FillScreen(0);
+    }
+
+    gfx_End();
+}
