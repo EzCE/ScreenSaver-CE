650a4f47af5f58cdc016faba54a8b1cc771713ae
TIny_Hacker <thetiniesthacker@gmail.com>
2025-12-23 12:22:39 -0500

    Clock screensaver
    Co-Authored-By: RoccoLox Programs <roccoloxprograms2018@gmail.com>

--- animations/src/aod.c
+++ animations/src/aod.c
@@ -12,11 +12,12 @@ bool aod(void) {
     gfx_Begin();
     gfx_SetColor(255);
     gfx_SetDrawBuffer();
+    gfx_ZeroScreen();
 
-    uint16_t date[3];
-    boot_GetDate(&date[0], &date[1], &date[2]);
+    uint8_t date[2];
+    boot_GetDate(&date[0], &date[1], NULL);
 
-    const char months[12] = {
+    const char months[12][4] = {
         "Jan",
         "Feb",
         "Mar",
@@ -34,6 +35,10 @@ bool aod(void) {
     gfx_SetTextBGColor(0);
     gfx_SetTextFGColor(255);
     gfx_SetTextTransparentColor(0);
+    gfx_FillRectangle_NoClip(160, 162, 3, 3);
+    gfx_PrintStringXY(months[date[1] - 1], 167, 160);
+    gfx_SetTextXY(194, 160);
+    gfx_PrintUInt(date[0], 1);
 
     while (!kb_AnyKey()) {
         if (utility_ChkAPDTimer()) {
@@ -42,15 +47,22 @@ bool aod(void) {
         }
 
         gfx_SetColor(0);
-        gfx_FillRectangle_NoClip(118, 78, 85, 85);
+        gfx_FillRectangle_NoClip(118, 73, 85, 85);
+        gfx_FillRectangle_NoClip(122, 160, 33, 7);
         gfx_SetColor(255);
-        gfx_FillCircle_NoClip(160, 110, 36);
+        gfx_FillCircle_NoClip(160, 114, 36);
         gfx_SetColor(0);
-        gfx_FillCircle_NoClip(160, 110, 32);
+        gfx_FillCircle_NoClip(160, 114, 32);
+        gfx_SetColor(192);
+        gfx_Line_NoClip(160, 114, 160 + 41 * cos(((double)rtc_Seconds / 60) * 2 * M_PI - M_PI / 2), 114 + 41 * sin(((double)rtc_Seconds / 60) * 2 * M_PI - M_PI / 2));
         gfx_SetColor(255);
-        // gfx_Line_NoClip(160, 110, 160 + 42 * cos(((double)rtc_Seconds / 60) * 2 * M_PI - M_PI / 2), 110 + 42 * sin(((double)rtc_Seconds / 60) * 2 * M_PI - M_PI / 2));
-        gfx_Line_NoClip(160, 110, 160 + 42 * cos((((double)rtc_Minutes * 60 + (double)rtc_Seconds) / 3600) * 2 * M_PI - M_PI / 2), 110 + 42 * sin((((double)rtc_Minutes * 60 + (double)rtc_Seconds) / 3600) * 2 * M_PI - M_PI / 2));
-        gfx_Line_NoClip(160, 110, 160 + 26 * cos((((double)rtc_Hours * 3600 + (double)rtc_Minutes * 60 + (double)rtc_Seconds) / 43200) * 2 * M_PI - M_PI / 2), 110 + 26 * sin((((double)rtc_Hours * 3600 + (double)rtc_Minutes * 60 + (double)rtc_Seconds) / 43200) * 2 * M_PI - M_PI / 2));
+        gfx_Line_NoClip(160, 114, 160 + 41 * cos((((double)rtc_Minutes * 60 + (double)rtc_Seconds) / 3600) * 2 * M_PI - M_PI / 2), 114 + 41 * sin((((double)rtc_Minutes * 60 + (double)rtc_Seconds) / 3600) * 2 * M_PI - M_PI / 2));
+        gfx_Line_NoClip(160, 114, 160 + 26 * cos((((double)rtc_Hours * 3600 + (double)rtc_Minutes * 60 + (double)rtc_Seconds) / 43200) * 2 * M_PI - M_PI / 2), 114 + 26 * sin((((double)rtc_Hours * 3600 + (double)rtc_Minutes * 60 + (double)rtc_Seconds) / 43200) * 2 * M_PI - M_PI / 2));
+        gfx_SetTextXY((rtc_Hours < 10 ? 129 : 122), 160);
+        gfx_PrintUInt(rtc_Hours, 1);
+        gfx_PrintChar(':');
+        gfx_PrintUInt(rtc_Minutes, 2);
+
         gfx_BlitBuffer();
     }
 
