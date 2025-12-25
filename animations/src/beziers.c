86d8980db794848b54b724fd5fe26afc3b428efa
Shaun McFall <shaun@shaunmcfall.com>
2025-12-25 00:47:28 -0500

    Additional control point, less trails, looks better

--- animations/src/beziers.c
+++ animations/src/beziers.c
@@ -8,8 +8,8 @@
 #include "utility.h"
 #include "palette.h"
 
-#define NUM_CURVES 7
-#define BEZIER_SEGMENTS 12
+#define NUM_CURVES 3
+#define BEZIER_SEGMENTS 32
 
 typedef struct Bezier {
     uint16_t x0, y0;
@@ -17,6 +17,7 @@ typedef struct Bezier {
     uint16_t x2, y2;
     uint16_t x3, y3;
     uint16_t x4, y4;
+    uint16_t x5, y5;
     uint16_t color;
 } Bezier;
 
@@ -29,8 +30,10 @@ void bezier_init(const uint8_t numCurves, Bezier curves[numCurves]) {
     curves[0].y2 = random() % GFX_LCD_HEIGHT;
     curves[0].x3 = random() % GFX_LCD_WIDTH;
     curves[0].y3 = random() % GFX_LCD_HEIGHT;
-    curves[0].x4 = curves[0].x0;
-    curves[0].y4 = curves[0].y0;
+    curves[0].x4 = random() % GFX_LCD_WIDTH;
+    curves[0].y4 = random() % GFX_LCD_HEIGHT;
+    curves[0].x5 = curves[0].x0;
+    curves[0].y5 = curves[0].y0;
     curves[0].color = random() % 255 + 1;
     
     for (uint8_t i = 1; i < numCurves; i++) {
@@ -44,6 +47,8 @@ void bezier_init(const uint8_t numCurves, Bezier curves[numCurves]) {
         curves[i].y3 = curves[0].y3;
         curves[i].x4 = curves[0].x4;
         curves[i].y4 = curves[0].y4;
+        curves[i].x5 = curves[0].x5;
+        curves[i].y5 = curves[0].y5;
         curves[i].color = curves[0].color;
     }
 }
@@ -57,19 +62,22 @@ void bezier_draw_curve(const Bezier *curve) {
         uint16_t t2 = (t * t) >> 8;
         uint16_t t3 = (t2 * t) >> 8;
         uint16_t t4 = (t3 * t) >> 8;
+        uint16_t t5 = (t4 * t) >> 8;
         uint16_t mt = 256 - t;
         uint16_t mt2 = (mt * mt) >> 8;
         uint16_t mt3 = (mt2 * mt) >> 8;
         uint16_t mt4 = (mt3 * mt) >> 8;
+        uint16_t mt5 = (mt4 * mt) >> 8;
         
-        uint16_t b0 = mt4;
-        uint16_t b1 = (4 * ((mt3 * t) >> 8));
-        uint16_t b2 = (6 * ((mt2 * t2) >> 8));
-        uint16_t b3 = (4 * ((mt * t3) >> 8));
-        uint16_t b4 = t4;
+        uint16_t b0 = mt5;
+        uint16_t b1 = (5 * ((mt4 * t) >> 8));
+        uint16_t b2 = (10 * ((mt3 * t2) >> 8));
+        uint16_t b3 = (10 * ((mt2 * t3) >> 8));
+        uint16_t b4 = (5 * ((mt * t4) >> 8));
+        uint16_t b5 = t5;
         
-        int16_t x = (b0 * curve->x0 + b1 * curve->x1 + b2 * curve->x2 + b3 * curve->x3 + b4 * curve->x4) >> 8;
-        int16_t y = (b0 * curve->y0 + b1 * curve->y1 + b2 * curve->y2 + b3 * curve->y3 + b4 * curve->y4) >> 8;
+        int16_t x = (b0 * curve->x0 + b1 * curve->x1 + b2 * curve->x2 + b3 * curve->x3 + b4 * curve->x4 + b5 * curve->x5) >> 8;
+        int16_t y = (b0 * curve->y0 + b1 * curve->y1 + b2 * curve->y2 + b3 * curve->y3 + b4 * curve->y4 + b5 * curve->y5) >> 8;
         
         gfx_Line(prevX, prevY, x, y);
         prevX = x;
@@ -111,7 +119,8 @@ void bezier_update(const uint8_t numCurves, Bezier curves[numCurves],
     int16_t *dx1, int16_t *dy1, 
     int16_t *dx2, int16_t *dy2,
     int16_t *dx3, int16_t *dy3,
-    int16_t *dx4, int16_t *dy4) {
+    int16_t *dx4, int16_t *dy4,
+    int16_t *dx5, int16_t *dy5) {
 
     for (int i = numCurves - 1; i > 0; i--) {
         curves[i].x0 = curves[i - 1].x0;
@@ -124,6 +133,8 @@ void bezier_update(const uint8_t numCurves, Bezier curves[numCurves],
         curves[i].y3 = curves[i - 1].y3;
         curves[i].x4 = curves[i - 1].x4;
         curves[i].y4 = curves[i - 1].y4;
+        curves[i].x5 = curves[i - 1].x5;
+        curves[i].y5 = curves[i - 1].y5;
         curves[i].color = curves[i - 1].color - 10;
     }
 
@@ -132,9 +143,10 @@ void bezier_update(const uint8_t numCurves, Bezier curves[numCurves],
     bezier_update_point(&curves[0].x2, &curves[0].y2, dx2, dy2);
     bezier_update_point(&curves[0].x3, &curves[0].y3, dx3, dy3);
     bezier_update_point(&curves[0].x4, &curves[0].y4, dx4, dy4);
+    bezier_update_point(&curves[0].x5, &curves[0].y5, dx5, dy5);
 }
 
-#define BEZIER_NEW_SPEED() (int16_t)((random() % 4 + 1) * 2.5f)
+#define BEZIER_NEW_SPEED() (int16_t)((random() % 4 + 1) * 1)
 
 bool beziers(void) {
     gfx_Begin();
@@ -161,8 +173,11 @@ bool beziers(void) {
     int16_t dx3 = BEZIER_NEW_SPEED(); 
     int16_t dy3 = BEZIER_NEW_SPEED();
 
-    int16_t dx4 = dx0;
-    int16_t dy4 = dy0;
+    int16_t dx4 = BEZIER_NEW_SPEED(); 
+    int16_t dy4 = BEZIER_NEW_SPEED();
+
+    int16_t dx5 = dx0;
+    int16_t dy5 = dy0;
 
     while (!kb_AnyKey()) {
         if (utility_ChkAPDTimer()) {
@@ -179,7 +194,8 @@ bool beziers(void) {
             &dx1, &dy1, 
             &dx2, &dy2, 
             &dx3, &dy3, 
-            &dx4, &dy4
+            &dx4, &dy4,
+            &dx5, &dy5
         );
         palette_shift(gfx_palette);
         
